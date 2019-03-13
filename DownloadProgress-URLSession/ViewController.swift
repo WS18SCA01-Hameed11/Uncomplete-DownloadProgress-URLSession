5e3c99687c137eb0037c0ed846363306aec6a59e
Hameed Abdullah <hameedabdullah@hameeds-macbook-pro.local>
2019-03-13 00:20:59 -0400

    Initial Commit

---
+++ DownloadProgress-URLSession/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DownloadProgress-URLSession
+//
+//  Created by Hameed Abdullah on 3/13/19.
+//  Copyright © 2019 Hameed Abdullah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
